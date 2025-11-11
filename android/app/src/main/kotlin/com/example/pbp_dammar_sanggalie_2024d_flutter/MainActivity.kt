85f817aae0ae736449c10c5dd175e48615a40d53
DinkyTrady <DinkyTrady@kyra.com>
2025-11-11 13:51:43 +0700

    push fix

--- android/app/src/main/kotlin/com/example/pbp_dammar_sanggalie_2024d_flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pbp_dammar_sanggalie_2024d_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/repositories/book_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:perpustakaan_mini/models/book_model.dart';
-
-abstract class BookRepository {
-  Future<List<DigitalBook>> searchBooks(String query);
-  Future<List<DigitalBook>> getInitialBooks(
-      {required int offset, required int number});
-  Future<DigitalBook> getBookDetails(int bookId);
-}
